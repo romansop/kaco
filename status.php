b1377c8655a0a1a5c932583ae24fc113ea57ea22
Roman Soprun <roman.soprun@globallogic.com>
2019-03-07 18:43:41 +0200

    Colorized version of status.php

--- status.php
+++ status.php
@@ -14,13 +14,64 @@
                 $date_ts = strtotime($date);
                 $yesterday = date("Y-m-d", $date_ts - 24 * 3600);
                 $tomorrow = date("Y-m-d", $date_ts + 24 * 3600);
-                echo "<div><a href='?date=$yesterday'>&lArr; $yesterday</a></div>";
-                echo "<div>$date</div>";
+                echo "<div><a href='?date=$yesterday'>&lArr; $yesterday</a></div>";                
+                echo "<div><a href='?was=$date'>$date</a></div>";
                 echo "<div><a href='?date=$tomorrow'>$tomorrow &rArr;</a></div>";
             ?>
             </div>
         <table border="1">
         <?php
+        
+            $gradient = [
+                "#FF0000",
+                "#FF1000",
+                "#FF2000",
+                "#FF3000",
+                "#FF4000",
+                "#FF5000",
+                "#FF6000",
+                "#FF7000",
+                "#FF8000",
+                "#FF9000",
+                "#FFA000",
+                "#FFB000",
+                "#FFC000",
+                "#FFD000",
+                "#FFE000",
+                "#FFF000",
+                "#FFFF00",
+                "#F0FF00",
+                "#E0FF00",
+                "#D0FF00",
+                "#C0FF00",
+                "#B0FF00",
+                "#A0FF00",
+                "#90FF00",
+                "#80FF00",
+                "#70FF00",
+                "#60FF00",
+                "#50FF00",
+                "#40FF00",
+                "#30FF00",
+                "#20FF00",
+                "#10FF00"
+            ];
+            
+            function getIntensityColor($intensity)
+            {
+                global $gradient;
+                
+                if ($intensity > 1)
+                    $intensity = 1;
+                $len = count($gradient);
+                $idx = floor($len * (1 - $intensity));
+                
+                if ($idx >= $len)
+                    return "#66ffff";
+                
+                return $gradient[$idx];
+            }
+            
             function map_data($arr)
             {
                 $data['time'] = $arr[0];
@@ -35,6 +86,8 @@
                 $data['vac1']  = round((double)$arr[8]);
                 $data['vac2']  = round((double)$arr[9]);
                 $data['vac3']  = round((double)$arr[10]);
+                $data['vac3']  = round((double)$arr[10]);
+                $data['space'] = "";
                 $data['vmac1'] = round((double)$arr[24]);
                 $data['vmac2'] = round((double)$arr[25]);
                 $data['vmac3'] = round((double)$arr[26]);
@@ -46,15 +99,18 @@
             {
                 $str = "";
                 
+                if ($code == "")
+                    return "";
+                
                 switch ((int)$code) {
                     case 0:
                         $str = "Запуск системи";
                         break;
                     case 1:
-                        $str = "Недостатнє освітлення панелей";
+                        $str = "Підготовка до генерації (недостатній струм від панелей)";
                         break;
                     case 2:
-                        $str = "Мала напруга на панелях";
+                        $str = "Недостатнє освітлення панелей (недостатня напруга)";
                         break;
                     case 4:
                         $str = ""; // Feed-in mode
@@ -62,10 +118,19 @@
                     case 7:
                     case 8:
                     case 75:
-                        $str = "Перевірка 3-х фазної мережі";
+                        $str = "Перевірка 3-х фазної мережі перед запуском";
                         break;
                     case 20:
                         $str = "Поступове підняття потужності генерації";
+                        break;
+                    case 42:
+                        $str = "Надто висока напруга на першій фазі";
+                        break;
+                    case 44:
+                        $str = "Надто висока напруга на другій фазі";
+                        break;
+                    case 46:
+                        $str = "Надто висока напруга на третій фазі";
                         break;
                     case 47:
                         $str = "Недопустима міжфазна напруга (перекіс фаз)";
@@ -111,10 +176,35 @@
                         if ($value != "")
                             $value = substr($value, 0, -5);
                     }
-                    if ($key == "status")
-                        echo "<td>" . $value . "</td>";
-                    else
-                        echo "<td>" . htmlspecialchars($value) . "</td>";
+                    
+                    $bgcolor = "";
+                    if (in_array($key, ["vac1","vac2","vac3","vmac1","vmac2","vmac3"])) {
+                        $voltage = round($value);
+                        if ($voltage >= 255)
+                            $bgcolor = ' bgcolor="#ff3300"';
+                        else if ($voltage >= 250)
+                            $bgcolor = ' bgcolor="#ff9900"';
+                        else if ($voltage >= 242)
+                            $bgcolor = ' bgcolor="#ffcc00"';
+                        else if ($voltage >= 232)
+                            $bgcolor = ' bgcolor="#ffff00"';                        
+                        else if ($voltage < 209)
+                            $bgcolor = ' bgcolor="#00ffff"';
+                        else if ($voltage < 232)
+                            $bgcolor = ' bgcolor="#ccff33"';
+                    }
+                    
+                    if ($key == "wac") {
+                        $intensity = (double)$value / 10000;
+                        $bgcolor = ' bgcolor="'.getIntensityColor($intensity).'"';
+                        //$bgcolor = ' bgcolor="#ffcc00"';
+                    }
+                    
+                    if ($key == "status") {
+                        echo "<td>" . $value . "</td>";                    
+                    } else {
+                        echo "<td".$bgcolor.">" . htmlspecialchars($value) . "</td>";
+                    }
                 }
                 echo "</tr>\n";
             }
@@ -126,8 +216,9 @@
             }
             $file = fopen($filename,"r");
             print_row(["Time","V DC I","A DC I", "W DC I", "V DC II",
-                "A DC II", "W DC II", "W AC", "Status", "V AC 1",  "V AC 2",  "V AC 3",  "Vmax AC 1",  "Vmax AC 2",  "Vmax AC 3"]);
-            $total = 0;            
+                "A DC II", "W DC II", "W AC", "Status", "V AC 1",  "V AC 2",  "V AC 3", "<--->",  "Vmax AC 1",  "Vmax AC 2",  "Vmax AC 3"]);
+
+            $total = 0;
             $last_handled = false;
             while(!feof($file) || !$last_handled)
             {     
@@ -149,15 +240,92 @@
                                 
                 if ($data['vdc1'] || $data['vdc2']) {
                     print_row($data);
-                    $total += $data['wac'];
                 }                    
             }            
             fclose($file);
+            
+            function print_per_hour_table()
+            {
+                global $total;
+                global $filename;
+                
+                $file = fopen($filename,"r");
+                //print_row(["Time","V DC I","A DC I", "W DC I", "V DC II",
+                //    "A DC II", "W DC II", "W AC"]);
+                
+                $avg = map_data([0,0,0,0,0,0,0,0]);
+                $count = 0;
+                $prev_h = 0;
+                $prev_ts = 0;
+                $last_handled = false;
+                while(!feof($file) || !$last_handled)
+                {     
+                    if (!feof($file)) {
+                        $arr = fgetcsv($file, 0, ",");
+                        if (!$arr) {
+                            continue;
+                        }
+
+                        $data = map_data($arr);
+
+                        $ts = floor(strtotime($arr[0]) / 60);
+                        if (!$prev_ts) {
+                            $prev_ts = $ts - 1;
+                        }
+
+                        $diff_ts = $ts - $prev_ts;
+                        $prev_ts = $ts;
+                        $hour = date("H", $ts * 60);
+                        if (!$prev_h) {
+                            $prev_h = $hour;
+                        }
+
+                        foreach ($avg as $key => $value) {
+                            $avg[$key] += $data[$key] * $diff_ts;
+                            if ($key == "wac")
+                                break;
+                        }
+                        $count += $diff_ts;
+                    } else {
+                        $last_handled = true;
+                        $hour++;
+                    }
+
+                    if ($prev_h != $hour) {  
+                        $data['time'] = $prev_h;
+                        foreach ($avg as $key => $value) {
+                            if ($key == 'time')
+                                continue;
+                            if (in_array($key, ["wac","wdc1","wdc2"])) {
+                                $data[$key] = $avg[$key] / 60;
+                            } else {
+                                if ($count != 0) {
+                                    $data[$key] = $avg[$key] / $count;
+                                } else {
+                                    $data[$key] = 0;
+                                }
+                            }
+                        }
+                        if ($data['wac']) {
+                            //print_row($data);
+                            $total += $data['wac'];
+                        }
+                        foreach ($avg as $key => $value) {
+                            $avg[$key] = 0;
+                        }
+                        $count = 0;
+                        $prev_h = $hour;
+                    }
+                }            
+                fclose($file);
+            }
+            
+            print_per_hour_table();
         ?>            
         </table>
         <?php
-            //$total = round($total) / 1000;
-            //echo "<br><div>Total: $total</div>";
+            $total = round($total) / 1000;
+            echo "<br><div>Total: $total</div>";
         ?>
         </div>
     </body>
